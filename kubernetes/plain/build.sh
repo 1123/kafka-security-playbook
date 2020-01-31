bd57bcd5f256d4dd87c529e57e82327e0d379115
Benedikt Linse <benedikt@confluent.io>
2020-01-31 10:09:06 +0100

    parameterising project

--- kubernetes/plain/build.sh
+++ kubernetes/plain/build.sh
@@ -1,3 +1,7 @@
-gcloud builds submit --tag gcr.io/solutionsarchitect-01/kafka-sasl-plain kafka/
-gcloud builds submit --tag gcr.io/solutionsarchitect-01/zookeeper-sasl-plain zookeeper/
+#!/bin/bash
+
+set -e -u
+
+gcloud builds submit --tag gcr.io/${PROJECT}/kafka-sasl-plain kafka/
+gcloud builds submit --tag gcr.io/${PROJECT}/zookeeper-sasl-plain zookeeper/
 
