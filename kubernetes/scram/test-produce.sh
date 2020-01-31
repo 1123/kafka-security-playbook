7ac294282e2dd1a6adb968251d3300980e0df2e8
Benedikt Linse <benedikt@confluent.io>
2020-01-31 14:46:08 +0100

    Adding README.md; formatting produce script

--- kubernetes/scram/test-produce.sh
+++ kubernetes/scram/test-produce.sh
@@ -1,2 +1,12 @@
-kubectl exec kafka -- kafka-producer-perf-test --record-size 150 --throughput -10 --num-records 10 --producer-props bootstrap.servers=kafka:9093 --topic test --producer.config /etc/kafka/consumer.properties
-# kubectl exec kafka -- echo "message-1" | kafka-console-producer --broker-list kafka:9093 --producer.config /etc/kafka/consumer.properties --topic test
+#!/bin/bash 
+
+set -e -u
+
+kubectl exec kafka -- \
+  kafka-producer-perf-test \
+    --record-size 150 \
+    --throughput -10 \
+    --num-records 10 \
+    --producer-props bootstrap.servers=kafka:9093 \
+    --topic test \
+    --producer.config /etc/kafka/consumer.properties
