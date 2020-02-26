a3a87b3e0b155458f5e6809d3ed2814d1f9c0acd
Benedikt Linse <benedikt@confluent.io>
2020-02-26 15:08:46 +0100

    adding restproxy to setup

--- kubernetes/rbac/build.sh
+++ kubernetes/rbac/build.sh
@@ -3,4 +3,6 @@
 set -e -u
 
 gcloud builds submit --tag gcr.io/solutionsarchitect-01/kafka-rbac kafka/
+gcloud builds submit --tag gcr.io/solutionsarchitect-01/cp-enterprise-control-center-rbac control-center/
+gcloud builds submit --tag gcr.io/solutionsarchitect-01/cp-kafka-rest-rbac kafka-rest/
 
